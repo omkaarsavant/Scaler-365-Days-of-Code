1b846a37429553dc918606abb199d12364c02b45
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-01-02 19:21:41 +0530

    Stdin and Stdout
    Fast Track Java #1

---
+++ Stdin and Stdout.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        s.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
