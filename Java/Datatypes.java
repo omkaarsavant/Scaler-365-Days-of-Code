8ce31010d19979dd3a59a534a90203a3b657d914
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-01-03 19:59:18 +0530

    Day 2

---
+++ Java/Datatypes.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String mystring1="InterviewBit";
+        String mystring2="Don't change the variable name";
+        int myint=11;
+        boolean myboolean=true;
+        
+        /*Don't change the code below*/
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+        
+    }
+}
