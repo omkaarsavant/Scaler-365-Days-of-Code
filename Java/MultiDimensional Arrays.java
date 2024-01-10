179375e5157d9e0d35d2b570f696eafb5085f635
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-01-10 15:22:01 +0530

    Create MultiDimensional Arrays.java

---
+++ Java/MultiDimensional Arrays.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] res = new int[m];
+
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                res[j] += arr[i][j];
+            }
+        }
+
+        for (int k = 0; k < m; k++) {
+            System.out.print(res[k]+" ");
+        }
+    }
+}
