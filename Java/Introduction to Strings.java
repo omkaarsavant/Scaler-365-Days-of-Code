ee7f136881ce89e66b9acb7cb1df17bd9c3104af
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-01-21 21:28:47 +0530

    Create Introduction to Strings.java

---
+++ Java/Introduction to Strings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String A = scanner.next();
+        String B = scanner.next();
+        scanner.close();
+
+        System.out.println(A.length() + B.length());
+        
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        
+        System.out.println(A.toUpperCase() + " " + B.toUpperCase());
+    }
+}
