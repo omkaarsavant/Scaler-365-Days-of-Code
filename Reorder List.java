07b44db656e128b47ae9a6cce80a86cbe811bbbf
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-03-09 22:12:38 +0530

    Day 68

---
+++ Reorder List.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reorderList(ListNode A) {
+        if (A == null || A.next == null) return A;
+        
+        ListNode slow = A, fast = A;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+    
+        ListNode prev = null, curr = slow, temp;
+        while (curr != null) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        // Step 3: Merge the two halves of the list
+        ListNode first = A, second = prev;
+        while (second.next != null) {
+            temp = first.next;
+            first.next = second;
+            first = temp;
+            
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+        
+        return A;
+    }
+}
