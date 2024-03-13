b31ca309758578863956db9ef68330811863002a
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-03-13 20:39:09 +0530

    Day 73

---
+++ Remove Duplicates from Sorted List.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ListNode deleteDuplicates(ListNode A) {
+        ListNode current = A;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return A;
+    }
+}
