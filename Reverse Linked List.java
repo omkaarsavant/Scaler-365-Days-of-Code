63c90a43b01142741c3912372c6f4addc161054b
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-03-16 19:58:43 +0530

    Day 75

---
+++ Reverse Linked List.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        ListNode prev = null;
+        ListNode current = A;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
