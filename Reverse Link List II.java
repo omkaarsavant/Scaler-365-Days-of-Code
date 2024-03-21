ae2eb2f756a3b0e1448ab37367c9b01cc16fcb46
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-03-21 23:41:20 +0530

    Day 80

---
+++ Reverse Link List II.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public ListNode reverseBetween(ListNode A, int B, int C) {
+        if (A == null) return null;
+        ListNode dummy = new ListNode(0); 
+        dummy.next = A;
+        ListNode pre = dummy; 
+        for (int i = 0; i < B - 1; i++) pre = pre.next;
+        
+        ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+        ListNode then = start.next; // a pointer to a node that will be reversed
+        
+        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+        for (int i = 0; i < C - B; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+        
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+        
+        return dummy.next;
+    }
+}
