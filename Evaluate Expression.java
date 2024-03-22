f6a7e2dfc6f14a4bf985c69d07fad94bb2d3b860
Omkar Savant <122353858+omkaarsavant@users.noreply.github.com>
2024-03-22 22:44:14 +0530

    Day 81

---
+++ Evaluate Expression.java
@@ -0,0 +1,34 @@
+
+
+public class Solution {
+    public int evalRPN(String[] A) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : A) {
+            if (isOperator(token)) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+}
